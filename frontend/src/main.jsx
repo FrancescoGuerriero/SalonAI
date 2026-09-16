cc81f15a9b3642c7191bdc1010cdaca0c63638ca
Francesco Guerriero <59146443+FrancescoGuerriero@users.noreply.github.com>
2026-09-16 12:56:57 +0100

    refactor(errors): load design tokens before feature styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,6 +9,7 @@ import { BookingProvider } from "./context/BookingContext.jsx";
 import { CartProvider } from "./context/CartContext.jsx";
 
 import "./index.css";
+import "./styles/salonTheme.css";
 import "./commerce.css";
 import "./styles/managementExperience.css";
 import "./styles/commerceExperience.css";
@@ -24,7 +25,6 @@ import "./styles/manageAccount.css";
 import "./styles/profileMedia.css";
 import "./styles/about.css";
 import "./styles/staffProfile.css";
-import "./styles/salonTheme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
