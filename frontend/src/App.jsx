7cca3f9b8cbd8e858dd0fca05fb137fd0ca451b6
Francesco Guerriero <59146443+FrancescoGuerriero@users.noreply.github.com>
2026-09-21 17:19:48 +0100

    Integrate Developer 3 frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1126,6 +1126,14 @@ function AppRoutes() {
         />
 
         <Route
+          path="admin/employees/record/:recordId"
+          element={permissionPage(
+            AdminEmployeeDetailPage,
+            "employee:read"
+          )}
+        />
+
+        <Route
           path="admin/employees/:id"
           element={permissionPage(
             AdminEmployeeDetailPage,
