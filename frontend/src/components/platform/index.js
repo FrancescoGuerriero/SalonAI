0f83c64a183ce93c56a8463dfc14f8a62d523acb
Francesco Guerriero <59146443+FrancescoGuerriero@users.noreply.github.com>
2026-09-26 10:33:18 +0100

    feat(dev2): export platform presentation components

---
+++ frontend/src/components/platform/index.js
@@ -0,0 +1,5 @@
+export { default as AIProposalDecision } from "./AIProposalDecision.jsx";
+export { default as CapabilityStatus } from "./CapabilityStatus.jsx";
+export { default as ConnectorHealth } from "./ConnectorHealth.jsx";
+export { default as EffectiveConfigurationRow } from "./EffectiveConfigurationRow.jsx";
+export { default as LocationContext } from "./LocationContext.jsx";
